727f0a4fd20649ac8dc92200762c51ade0d55c8d
Pipirongcha <pipirongcha0x0@gmail.com>
2025-11-26 20:16:25 +0900

    [FEAT] Add Customer's Abstract Class
    Customer의 추상 클래스 작성 (미완)

--- Assets/01_Scripts/Base/CustomerBase.cs
+++ Assets/01_Scripts/Base/CustomerBase.cs
@@ -6,13 +6,52 @@ public abstract class CustomerBase<T>: MonoBehaviour where T: MonoBehaviour
 {
     private int foodCnt;
     private FOODS food;
+    private int customerTypeLimit; //CustomerType 결정 중에 배달원을 추가 또는 제거할 수 있도록 해주는 필드값
+    private CUSTOMERS customerType;
+    private int foodCntLimit;
+    private float patienceGauge; //손님의 인내심 게이지, 0이 되면 나가버림
+    private float patienceDownSpeed;
 
+    public void Start()
+    {
+        if (GameManager.Instance.isDeliveryWaiting) //@TODO: 의존성 제거 방법 고민해보기
+        {
+            customerTypeLimit = 3;
+        }
+        else
+        {
+            customerTypeLimit = 4;
+        }
+
+        customerType = (CUSTOMERS)Random.Range(1, customerTypeLimit);
+
+
+            switch (customerType)
+            {
+                case CUSTOMERS.TableCustomer:
+                    foodCntLimit = 2;
+                    break;
+                case CUSTOMERS.TableGlutton:
+                    foodCntLimit = 3;
+                    break;
+                case CUSTOMERS.Delivery:
+                    foodCntLimit = 4; //@TODO: 임시값, 배달 랭크에 따라서 오르도록 수정하기
+                    break;
+            }
+        foodCnt = Random.Range(1, foodCntLimit);
+        food = (FOODS)Random.Range(1, 7);
+    }
 
     public void OnEnable()
     {
-        foodCnt = Random.Range(0, 3);
+        
     }
-    private void OrderingFood()
+
+    private void WaitingWater()
+    {
+
+    }
+    private void WaitingFood()
     {
         // 손님이라면? 물을 받았을 때 호출됨
         // 배달부라면? 바로 호출
@@ -26,4 +65,29 @@ public abstract class CustomerBase<T>: MonoBehaviour where T: MonoBehaviour
     }
 
     public abstract int PayingFood();
+
+    private void Update()
+    {
+        if (customerType == CUSTOMERS.Delivery) return;
+
+        patienceGauge -= Time.deltaTime * patienceDownSpeed;
+
+        if (patienceGauge <= 0f)
+        {
+            patienceGauge = 0f;
+            TableOut();
+        }
+
+        UpdatePatienceUI();
+    }
+
+    private void UpdatePatienceUI()
+    {
+        //@TODO: 손님 인내심 게이지 UI 업데이트
+    }
+
+    private void TableOut()
+    {
+        //@TODO: 손님 화나서 나가는 연출
+    }
 }
---
+++ Assets/01_Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public bool isDeliveryWaiting;
+    private float deliveryPeriod; //배달원 방문 주기
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
